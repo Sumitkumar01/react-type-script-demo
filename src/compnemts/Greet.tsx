cab55a444c0edba38f579b59ce08d77daf926dd0
sumit <sk7627577@gmail.com>
2024-07-10 14:46:43 +0530

    initail commit & adding new component greet

---
+++ src/compnemts/Greet.tsx
@@ -0,0 +1,17 @@
+
+type GreetProps = {
+    name: string,
+    messageCount: number,
+    isLoggedIn: boolean,
+}
+
+
+const Greet = (props: GreetProps) => {
+    return (
+        <div>
+            <h1>{props.isLoggedIn ? `Welcome ${props.name}! You have ${props.messageCount} unread messages` : "Welcome Guest!"} </h1>
+        </div>
+    )
+}
+
+export default Greet;
